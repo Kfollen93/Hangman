edfc4d622b0d341283d6141d4c3bb0abfd2e39d7
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-22 16:55:50 -0700

    Separted class into two methods

--- main.rb
+++ main.rb
@@ -9,16 +9,23 @@
 
 class Game
 
- def load_script
-  dictionary = File.open('5desk.txt', 'r')
-   dictionary.readlines.each do |word|
-    choose_word = word.gsub(/\s+/, '')
-    if choose_word.length.between?(5, 12)
-      puts Random.rand(choose_word)
+def load_script
+@dictionary = File.open('5desk.txt', 'r')
+  @dictionary.readlines.each do |word|
+  @@choose_word = word.gsub(/\s+/, '')
+    if @@choose_word.length.between?(5, 12)
+      @@choose_word
     end
-   end
- end
+  end
+end
+
+def random_word
+  @word_array = @@choose_word.split("\n")
+  puts @word_array.sample #how do I get just one random word to return
+end
+
 end
 
 start = Game.new
 start.load_script
+start.random_word
