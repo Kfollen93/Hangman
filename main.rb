3d3bb64366552a38cdc755eab999306c6d3d3762
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-22 19:09:00 -0700

    Removed old notes

--- main.rb
+++ main.rb
@@ -3,29 +3,3 @@
 
 #start = Game.new (so Game class/file should have initialize method?)
 
- 
-#GAME.RB FILE
-
-
-class Game
-
-def load_script
-@dictionary = File.open('5desk.txt', 'r')
-  @dictionary.readlines.each do |word|
-  @@choose_word = word.gsub(/\s+/, '')
-    if @@choose_word.length.between?(5, 12)
-      @@choose_word
-    end
-  end
-end
-
-def random_word
-  @word_array = @@choose_word.split("\n")
-  puts @word_array.sample #how do I get just one random word to return
-end
-
-end
-
-start = Game.new
-start.load_script
-start.random_word
