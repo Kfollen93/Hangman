8b3923be6b3557731ba40842675cf92526d89ce9
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-25 10:02:09 -0700

    Added start feature

--- main.rb
+++ main.rb
@@ -1,5 +1,10 @@
 #require_relative files
+require_relative 'display.rb'
+require_relative 'game.rb'
 
-
+display_game = Display.new
+display_game.instructions
+start = Game.new
+start.word(5, 12)
 #start = Game.new (so Game class/file should have initialize method?)
 
