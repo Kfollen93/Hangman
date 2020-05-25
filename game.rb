e23d91522025b0494f026e8dbd1e3564792b9c90
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-25 14:07:20 -0700

    Removed notes

--- game.rb
+++ game.rb
@@ -6,7 +6,6 @@ end
 
 def word(min, max)
   $choose_word = dictionary.select{ |word| word.length.between?(min, max) }.sample
-  #puts choose_word #this is a STRING   #don't want to display word, but here for testing
   $choose_word.chars #This is an ARRAY broken by each char.
 end
 
