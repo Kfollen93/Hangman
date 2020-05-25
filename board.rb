8e9f715376660f3eb0baddaf1d02b0e2e6a5e5fe
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-25 11:12:45 -0700

    Added loop for number of turns

--- board.rb
+++ board.rb
@@ -1,14 +1,27 @@
 class Board
 
-def game_board
+def initialize
+    #@slots = $choose_word  # _ _ _ appears for each char in choose_word variable
+    @slots = Array.new(12, nil)
 end
 
 def input_guess
-    guess = gets.chomp
-    until guess =~ /\A[a-z]{1}\z/ #Regex for guess as a String.
-    puts 'Your guess must only be one alphabetic character.'
-    guess = gets.chomp
+    1.upto(6) do |i|
+        puts "Turn #{i}: Type in one letter and press 'Enter'."
+        guess = gets.chomp
+        until guess =~ /\A[a-z]{1}\z/ #Regex for guess as a String.
+        puts 'Your guess must only be one alphabetic letter.'
+        guess = gets.chomp
+        end
     end
+end
+
+def to_s
+    @slots.map do |slots|
+        print slots || '_ '
+    end
+    puts " "
+    puts " "
 end
 
 end
