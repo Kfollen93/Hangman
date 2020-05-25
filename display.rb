b44c2c8d73fdff9b2c4edff7425f14e26ac9e337
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-25 11:17:38 -0700

    Removed methods, added comments

--- display.rb
+++ display.rb
@@ -1,26 +1,17 @@
-class Display
+class Display #this might change to a module and rename
 
 def instructions
  puts   <<~MYHEREDOC
 
         Classic Hangman, the rules are simple.
 
-    Computer sets a word, you guess one character per turn.
+    Computer sets a word, you guess one letter per turn.
         You have 6 turns to guess the world correctly.
 
     MYHEREDOC
 
-    "Crunching algorithms and setting word...\n".each_char { |c| putc c ; sleep 0. }
+    "Crunching algorithms and setting word...\n".each_char { |c| putc c ; sleep 0.10 }
     puts " "
-end
-
-def remaining_guesses
-end
-
-def correct_letters
-end
-
-def incorrect_letters
 end
 
 end
