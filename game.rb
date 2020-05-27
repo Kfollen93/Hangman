933c483ddc96f85b2681a3dee6d2a72d56166c15
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-27 11:52:40 -0700

    Added instance variable of word for lose statement

--- game.rb
+++ game.rb
@@ -3,6 +3,7 @@ class Game
 
   def setup
     word = choose_word # reminder, this is the method "choose_word"
+    @word = word # Added this to access it in lose statement.
     @board = Board.new(word)
     @used_letters = []
   end
@@ -14,7 +15,6 @@ class Game
   def choose_word
     word = dictionary.select { |word| word.length.between?(5, 12) }.sample
     chosen_word = word.downcase
-    # puts chosen_word # Just for testing.
     chosen_word
   end
 
@@ -23,16 +23,15 @@ class Game
     1.upto(6) do |i| # This will need to be changed to IF the letter is correct, it doesn't count as a turn
       puts "Turn #{i}: Type in one letter and press 'Enter'."
       @guess = gets.chomp
-      # Not sure what will happen if it's a correct letter that's entered twice yet.
       until @guess =~ /\A[a-z]{1}\z/ && !@used_letters.include?(@guess)
         puts 'Your guess must be one lowercase letter and not used before.'
         @guess = gets.chomp
       end
       board.update(@guess)
-      @used_letters.push(@guess)
+      p @used_letters.push(@guess)
       game_over?
       if i >= 6
-        puts "You lose. The word was: '#{@chosen_word}'."
+        puts "You lose. The word was: #{@word}."
       end
     end
   end
