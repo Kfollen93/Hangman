6256e77a2a87f3f196f0512f025177a520b37662
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-27 07:59:28 -0700

    Refactored two methods into one update method

--- board.rb
+++ board.rb
@@ -3,28 +3,19 @@ class Board
       @slots = word.chars
     end
   
-    def check_for_correct_guess
-      @slots.each_with_index do |value, index|
-        if @slots.include?(@guess[index]) #guess is just a letter, no index to reference
-          print @slots[index] = value
-        end
-      end
-    end
-  
-    def check_for_wrong_guess
-      unless @slots.include?(@guess)
-        @used_letters.push(@guess)
-        puts "Used letter(s): #{@used_letters}"
-        puts " "
-      end
-    end
-  
     def map_blanks
       print @slots.map { '_' }
       puts ' '
     end
 end
 
-
-def update #need to refactor the two guess methods into one update method
+def update
+  @slots.each_with_index do |value, index|
+    if @guess.include?(@slots[index])
+      print @slots[index] = value
+    elsif !@slots.include?(@guess)
+      @used_letters.push(@guess)
+      puts "Used letter(s): #{@used_letters}"
+      puts " "
+    end
 end
