429e8b33d686e6dcaad2cc4a6ea6f95553baeb71
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-05-29 16:43:43 -0700

    Format

--- display.rb
+++ display.rb
@@ -1,21 +1,22 @@
-class Display # this might need to change to a module and rename
+class Display
+  def instructions
+    puts <<~MYHEREDOC
 
-def instructions
- puts   <<~MYHEREDOC
+      #{blue('        Classic Hangman, the rules are simple.
 
-    #{blue('        Classic Hangman, the rules are simple.
-
-    Computer sets a word, you guess one letter per turn.
-        You have 6 turns to guess the word correctly.')}
+      Computer sets a word, you guess one letter per turn.
+          You have 6 turns to guess the word correctly.')}
 
     MYHEREDOC
     puts " "
-end
+  end
 
-# Color Methods
-def colorize(text, color_code); "\e[#{color_code}m#{text}\e[0m"; end
-def red(text); colorize(text, 31); end
-def green(text); colorize(text, 32); end
-def blue(text); colorize(text, 36); end
+  # Color Methods
+  def colorize(text, color_code); "\e[#{color_code}m#{text}\e[0m"; end
 
+  def red(text); colorize(text, 31); end
+
+  def green(text); colorize(text, 32); end
+
+  def blue(text); colorize(text, 36); end
 end
